6f55d841bb5f487cea695184c3859fc472218be2
Alexandre da Silva <alexandre.silva@wplex.com.br>
2017-01-26 11:04:42 -0200

    fixing layout

--- src/main/java/com/sky/controller/ProductSelectionController.java
+++ src/main/java/com/sky/controller/ProductSelectionController.java
@@ -45,7 +45,7 @@ public class ProductSelectionController
 
     @RequestMapping(value = "customerLocation", method = RequestMethod.POST)
     public String getCustomerLocation(@RequestBody
-    final MultiValueMap<String, Object> customer, final Model model)
+        final MultiValueMap<String, Object> customer, final Model model)
     {
         try
         {
@@ -55,7 +55,7 @@ public class ProductSelectionController
 
             final List<Product> availableProducts = this.catalogueService.getAvailableProducts(locationId);
 
-            model.addAttribute("message", "ola");
+            model.addAttribute("customerId", customerId);
             model.addAttribute("availableProducts", availableProducts);
         }
         catch (final CustomerNotFoundException | InvalidLocationException e)
